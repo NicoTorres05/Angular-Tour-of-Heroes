41c1773dcbc5e886d3be4ceb3dbb04fe70c75d30
Osho . <ntordel0105>
2025-02-17 20:02:26 +0100

    Display list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Angular-Tour-of-Heroes';
+  title = 'Tour of Heroes';
 }
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import {NgIf, UpperCasePipe} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import { HEROES } from '../mock-heroes';
+import {NgFor} from '@angular/common';
+
+@Component({
+  standalone: true,
+  selector: 'app-heroes',
+  imports: [
+    UpperCasePipe,
+    FormsModule,
+    NgFor,
+    NgIf
+  ],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class HeroesComponent {
+
+  heroes = HEROES;
+
+  hero: Hero = {
+    id: 1,
+    name: 'WindStorm'
+};
+ selectedHero?: Hero;
+ onSelect(hero: Hero): void {
+   this.selectedHero = hero;
+ }
+}
